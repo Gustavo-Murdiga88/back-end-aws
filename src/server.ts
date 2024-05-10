ed9e716eba5859d6bacf9c4b667b0dd7624a46d3
Gustavo Murdiga <gustavo.murdiga@guaranisistemas.com.br>
2024-05-10 19:18:41 -0300

    lib: add lib ts-up

--- src/server.ts
+++ src/server.ts
@@ -12,5 +12,5 @@ app.register(fastifyMultipart)
 app.register(FilesRoute)
 
 app.listen({ port: Number(process.env.PORT || 3333) }).then(() => {
-  console.log("HTTP server running on port 3333")
+  console.log(`HTTP server running on port ${process.env.PORT}`)
 });
